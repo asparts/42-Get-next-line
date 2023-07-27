c574cb068e4067e5d64b0ea85ea1535ca8fc1c3f
markus <nummimarkus@gmail.com>
2023-07-27 16:38:09 +0300

    norminette fixes

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,9 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 11:45:15 by mnummi            #+#    #+#             */
-/*   Updated: 2023/06/29 11:45:17 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/27 16:36:18 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
@@ -18,10 +16,10 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	if(fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) <0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	buffer = read_file(fd, buffer);
-	if(!buffer)
+	if (!buffer)
 		return (NULL);
 	line = ft_read_line(buffer);
 	buffer = ft_next_line(buffer);
